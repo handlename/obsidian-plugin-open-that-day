3c62b15ee2ab63835664486accc5f233ca53541f
handlename <nagata@handlena.me>
2024-02-17 23:03:37 +0900

    refactor: case for all locales in single table

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -20,52 +20,48 @@ describe('parse', () => {
 	});
 
 	describe.each([
-		["Today", today],
-		["3 weeks later", today.add(3, "w")]
-	])("locale = [en], parse '%s' as '%s", (word: string, date: Dayjs) => {
-		// test("check", () => {
-		const parser = new Parser(["en"]);
-		const res = parser.parse(word);
+		{
+			locales: ["en"],
+			text: "Today",
+			days: [today]
+		},
+		{
+			locales: ["en"],
+			text: "3 weeks later",
+			days: [today.add(3, "w")]
+		},
+		{
+			locales: ["ja"],
+			text: "昨日",
+			days: [today.subtract(1, "d")]
+		},
+		{
+			locales: ["ja"],
+			text: "明日",
+			days: [today.add(1, "d")]
+		},
+		{
+			locales: ["en", "ja"],
+			text: "昨日",
+			days: [today.subtract(1, "d")]
+		},
+		{
+			locales: ["en", "ja"],
+			text: "Tomorrow",
+			days: [today.add(1, "d")]
+		},
+	])("in locales %p, parse '%s'", ({ locales, text, days }) => {
+		const parser = new Parser(locales);
+		const results = parser.parse(text);
 
-		it("should returns 1 result", () => {
-			expect(res.length).toBe(1);
+		it(`should returns ${days.length} result(s)`, () => {
+			expect(results.length).toBe(1);
 		});
 
-		it(`should be parsed to ${date.format()}`, () => {
-			expect(res[0].format()).toBe(date.format());
-		})
-		// });
-	});
-
-	describe.each([
-		["昨日", today.subtract(1, "d")],
-		["明日", today.add(1, "d")],
-	])("locale = [ja], parse '%s' as '%s'", (word: string, date: Dayjs) => {
-		const parser = new Parser(["ja"]);
-		const res = parser.parse(word);
-
-		it("should returns 1 result", () => {
-			expect(res.length).toBe(1);
-		});
-
-		it(`should be parsed to ${date.format()}`, () => {
-			expect(res[0].format()).toBe(date.format());
-		});
-	});
-
-	describe.each([
-		["昨日", today.subtract(1, "d")],
-		["Tomorrow", today.add(1, "d")],
-	])("locale = [en, ja], parse '%s' as '%s'", (word: string, date: Dayjs) => {
-		const parser = new Parser(["en", "ja"]);
-		const res = parser.parse(word);
-
-		it("should returns 1 result", () => {
-			expect(res.length).toBe(1);
-		});
-
-		it(`should be parsed to ${date.format()}`, () => {
-			expect(res[0].format()).toBe(date.format());
+		it.each(
+			results.map((r, i) => [r, days[i]])
+		)('result %s should be %s', (result: Dayjs, expected: Dayjs) => {
+			expect(result.format()).toBe(expected.format());
 		});
 	});
 });
