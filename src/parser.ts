13b3d1e4c5a640d360b707c30551fc8f22ac7146
handlename <nagata@handlena.me>
2024-02-23 14:03:21 +0900

    refactor: move Result to a file

--- src/parser.ts
+++ src/parser.ts
@@ -1,39 +1,6 @@
 import * as chrono from "chrono-node";
 import dayjs, { Dayjs } from "dayjs";
-
-type Result<T, E extends Error> = Success<T> | Failure<E>
-
-class Success<T> {
-	readonly value: T;
-
-	constructor(value: T) {
-		this.value = value;
-	}
-
-	isSuccess(): this is Success<T> {
-		return true;
-	}
-
-	isFailure(): this is Failure<Error> {
-		return false;
-	}
-}
-
-class Failure<E extends Error> {
-	readonly error: E;
-
-	constructor(error: E) {
-		this.error = error;
-	}
-
-	isSuccess(): this is Success<unknown> {
-		return false;
-	}
-
-	isFailure(): this is Failure<E> {
-		return true;
-	}
-}
+import { Result, Success, Failure } from "./result";
 
 interface ParserFn {
 	(text: string, ref?: chrono.ParsingReference | Date, option?: chrono.ParsingOption): chrono.ParsedResult[];
