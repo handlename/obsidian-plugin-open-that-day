ebdbbfba7282999e7a801e0c0a8710014318a369
handlename <nagata@handlena.me>
2024-02-23 14:56:16 +0900

    style: format

--- src/result.ts
+++ src/result.ts
@@ -1,31 +1,33 @@
 export type Result<T, E extends Error> = Success<T> | Failure<E>;
+
 export class Success<T> {
-    readonly value: T;
+	readonly value: T;
 
-    constructor(value: T) {
-        this.value = value;
-    }
+	constructor(value: T) {
+		this.value = value;
+	}
 
-    isSuccess(): this is Success<T> {
-        return true;
-    }
+	isSuccess(): this is Success<T> {
+		return true;
+	}
 
-    isFailure(): this is Failure<Error> {
-        return false;
-    }
+	isFailure(): this is Failure<Error> {
+		return false;
+	}
 }
+
 export class Failure<E extends Error> {
-    readonly error: E;
+	readonly error: E;
 
-    constructor(error: E) {
-        this.error = error;
-    }
+	constructor(error: E) {
+		this.error = error;
+	}
 
-    isSuccess(): this is Success<unknown> {
-        return false;
-    }
+	isSuccess(): this is Success<unknown> {
+		return false;
+	}
 
-    isFailure(): this is Failure<E> {
-        return true;
-    }
+	isFailure(): this is Failure<E> {
+		return true;
+	}
 }
